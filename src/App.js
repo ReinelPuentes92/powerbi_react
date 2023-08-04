ad8b48675af2a1ab4d06360990b44deb73655c72
rfpuentes@indracompany.com <rfpuentes@indracompany.com>
2023-08-04 15:12:56 -0500

    base code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { models } from 'powerbi-client';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <PowerBIEmbed
+          embedConfig={{
+            type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+            id: '6bfb51c8-962d-4d89-b8b4-baa1e28ad927',
+            embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=6bfb51c8-962d-4d89-b8b4-baa1e28ad927&groupId=77084e6b-f3ce-40a3-97b6-94d7c49d3231&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUNFTlRSQUwtVVMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19',
+            accessToken: '<Access Token>',
+            tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+            settings: {
+              panes: {
+                filters: {
+                  expanded: false,
+                  visible: false
+                }
+              },
+              background: models.BackgroundType.Transparent,
+            }
+          }}
+
+          eventHandlers={
+            new Map([
+              ['loaded', function () { console.log('Report loaded'); }],
+              ['rendered', function () { console.log('Report rendered'); }],
+              ['error', function (event) { console.log(event.detail); }],
+              ['visualClicked', () => console.log('visual clicked')],
+              ['pageChanged', (event) => console.log(event)],
+            ])
+          }
+
+          cssClassName={"Embed-container"}
+
+          getEmbeddedComponent={(embeddedReport) => {
+            this.report = embeddedReport;
+          }}
+        />
       </header>
     </div>
   );
